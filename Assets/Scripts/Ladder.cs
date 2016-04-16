80f1e4945799352f895f4321fb870143226dd71e
Kasper Mol <awsumthx@gmail.com>
2016-04-16 13:47:07 +0200

    Updated ladder prefab

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -9,10 +9,12 @@ public class Ladder : MonoBehaviour {
 
 	private int length;
 	private Rigidbody2D body;
+	private SpriteRenderer segmentRenderer;
 
 	void Awake () {
 		length = startLength;
 		body = GetComponent<Rigidbody2D> ();
+		segmentRenderer = segmentPrefab.transform.Find ("sprite").GetComponent<SpriteRenderer> ();
 
 		BuildLadder ();
 	}
@@ -29,7 +31,6 @@ public class Ladder : MonoBehaviour {
 			return; // Nope
 
 		for (int i = 0; i < length - segments.Count; i++) {
-			SpriteRenderer segmentRenderer = segmentPrefab.transform.Find ("sprite").GetComponent<SpriteRenderer> ();
 			Vector2 segmentPosition = new Vector2 (
             	0,
 				segmentRenderer.bounds.size.y * (i + segments.Count)
@@ -46,6 +47,20 @@ public class Ladder : MonoBehaviour {
 		segment.transform.localPosition = position;
 		segment.transform.localRotation = Quaternion.identity;
 
+		// Update BoxCollider
+		BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+		Bounds ladderBounds = GetBounds ();
+
+		boxCollider.size = new Vector2 (
+			ladderBounds.size.x,
+			ladderBounds.size.y
+		);
+
+		boxCollider.offset = new Vector2 (
+			0f,
+			boxCollider.size.y * .5f - segmentRenderer.bounds.size.y * .5f
+		);
+
 		// Set camera follow target to latest segment, for now
 		Camera.main.GetComponent<SmoothCamera2D>().target = segment.transform;
 	}
@@ -58,6 +73,16 @@ public class Ladder : MonoBehaviour {
 			}
 		}
 		return children;
+	}
+
+	Bounds GetBounds(){
+		Bounds bounds = new Bounds(transform.position, Vector2.zero);
+
+		foreach(Renderer r in GetComponentsInChildren<SpriteRenderer>()) {
+			bounds.Encapsulate(r.bounds);
+		}
+
+		return bounds;
 	}
 
 	void Update () {
