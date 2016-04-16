cf6a163a6dc1a74952d85f7cfe30b39646a8cb2e
Kasper Mol <awsumthx@gmail.com>
2016-04-16 22:54:59 +0200

    WIP scrolling background horizontally

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -12,6 +12,8 @@ public class BackgroundScroller : MonoBehaviour {
 	private List<GameObject> spritePool;
 
 	void Start(){
+		spritePool = new List<GameObject> ();
+
 		startPosition = new Vector2 (
 			0,
 			Camera.main.transform.localPosition.y - Camera.main.orthographicSize
@@ -34,17 +36,65 @@ public class BackgroundScroller : MonoBehaviour {
 				-(spriteRenderer.bounds.size.x * .5f * (poolSize - 1)) + spriteRenderer.bounds.size.x * i,
 				0
 			);
+
+			spritePool.Add (spriteObject);
 		}
 	}
 
 	void Update (){
 		transform.position = new Vector3 (
-			startPosition.x + Camera.main.transform.position.x * scrollSpeed.x,
+			startPosition.x - Camera.main.transform.position.x * scrollSpeed.x,
 			startPosition.y + Camera.main.transform.position.y * scrollSpeed.y,
 			startPosition.z
 		);
 
 		// Check bounds and see if we need more sprites
+		CheckSpritePositions();
+	}
 
+	void CheckSpritePositions(){
+		if (spritePool.Count == 0)
+			return;
+		
+		Bounds cameraBounds = Camera.main.GetOrthographicBounds();
+		Bounds spriteBounds = GetBounds ();
+
+		Debug.Log (spriteBounds);
+
+		GameObject mostRightSprite = null;
+		GameObject mostLeftSprite = null;
+		foreach(GameObject sprite in spritePool){
+			if(mostRightSprite == null || sprite.transform.localPosition.x > mostRightSprite.transform.localPosition.x){
+				mostRightSprite = sprite;
+			}
+
+			if(mostLeftSprite == null || sprite.transform.localPosition.x < mostLeftSprite.transform.localPosition.x){
+				mostLeftSprite = sprite;
+			}
+		}
+
+		SpriteRenderer spriteRenderer = mostRightSprite.GetComponent<SpriteRenderer>();
+
+		if (cameraBounds.max.x > spriteBounds.max.x) {
+			mostLeftSprite.transform.localPosition = new Vector2 (
+				mostRightSprite.transform.localPosition.x + spriteRenderer.bounds.size.x,
+				0
+			);
+		} else if (cameraBounds.min.x < spriteBounds.min.x) {
+			mostRightSprite.transform.localPosition = new Vector2 (
+				mostLeftSprite.transform.localPosition.x - spriteRenderer.bounds.size.x,
+				0
+			);
+		}
+	}
+
+	Bounds GetBounds(){
+		Bounds bounds = new Bounds(transform.localPosition, Vector2.zero);
+
+		foreach(Renderer r in GetComponentsInChildren<SpriteRenderer>()) {
+			bounds.Encapsulate(r.bounds);
+		}
+
+		return bounds;
 	}
 }
