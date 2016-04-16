f1a77b585e2cf5dc89c0e87e2130748668489177
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-04-17 00:48:37 +0200

    Made camera smoothing a Vector2.

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SmoothCamera2D : MonoBehaviour {
 
-	public float followMultiplicative = 0.2f;
+	public Vector2 followMultiplicative = new Vector2(0.001f, 0.01f);
 	public Transform target;
 
 	void FixedUpdate () 
@@ -13,7 +13,7 @@ public class SmoothCamera2D : MonoBehaviour {
 		Vector2 delta = destination - origin; 
 		//Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 		//Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-		transform.position = transform.position + new Vector3 (delta.x, delta.y, 0) * (1 - Mathf.Pow(followMultiplicative, Time.deltaTime));
+		transform.position = transform.position + new Vector3 (delta.x * (1 - Mathf.Pow(followMultiplicative.x, Time.deltaTime)), delta.y  * (1 - Mathf.Pow(followMultiplicative.y, Time.deltaTime)), 0);
 		//Vector3 destination = transform.position + delta;
 		//transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		Debug.Log(destination.y + " " + transform.position.y + " " + delta.y);
