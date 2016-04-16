6c299a5a2e690eb6fbd65029e94968bf3e2cb2c3
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-04-16 23:37:11 +0200

    Made my player a prefab. Removed player.

--- Assets/Scripts/SmoothCamera2D.cs
+++ Assets/Scripts/SmoothCamera2D.cs
@@ -15,7 +15,7 @@ public class SmoothCamera2D : MonoBehaviour {
 			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			transform.position = transform.position + new Vector3 (delta.x, delta.y, 0) * Mathf.Pow(0.5f, Time.deltaTime);
-
+			Debug.Log (Mathf.Pow (0.5f, Time.deltaTime) + " " + new Vector3 (delta.x, delta.y, 0));
 			//Vector3 destination = transform.position + delta;
 			//transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}
