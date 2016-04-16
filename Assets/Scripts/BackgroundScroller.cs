34083915ca7b5acd7093a85169d0aadba5702dd4
Kasper Mol <awsumthx@gmail.com>
2016-04-16 22:25:49 +0200

    Create background sprite pool for horizontal repeating

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -1,13 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BackgroundScroller : MonoBehaviour {
 	public Vector2 scrollSpeed;
+	public Sprite sprite;
+
+	private int poolSize = 3; //We don't do this dynamically because we don't want to allocate memory during runtime
 
 	private Vector3 startPosition;
+	private List<GameObject> spritePool;
 
 	void Start(){
-		startPosition = transform.position;
+		startPosition = new Vector2 (
+			0,
+			Camera.main.transform.localPosition.y - Camera.main.orthographicSize
+		);
+
+		FillPool ();
+	}
+
+	void FillPool(){
+		for (var i = 0; i < poolSize; i++) {
+			GameObject spriteObject = new GameObject ();
+			SpriteRenderer spriteRenderer = spriteObject.AddComponent<SpriteRenderer> ();
+
+			spriteRenderer.sprite = sprite;
+		
+			spriteObject.transform.parent = transform;
+
+			// Position the sprite next to the last one in the pool
+			spriteObject.transform.localPosition = new Vector2 (
+				-(spriteRenderer.bounds.size.x * .5f * (poolSize - 1)) + spriteRenderer.bounds.size.x * i,
+				0
+			);
+		}
 	}
 
 	void Update (){
@@ -16,5 +43,8 @@ public class BackgroundScroller : MonoBehaviour {
 			startPosition.y + Camera.main.transform.position.y * scrollSpeed.y,
 			startPosition.z
 		);
+
+		// Check bounds and see if we need more sprites
+
 	}
 }
