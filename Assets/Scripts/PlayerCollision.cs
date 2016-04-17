3201ec2a56120c45eb0d7a3f49d25242b5fe6a89
Luigi van der Pal <eentweedrie@gmail.com>
2016-04-17 02:58:36 +0200

    Player collision and simple game over

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Collision(Collider2D other) {
+		Debug.Log (other.gameObject.tag);
+		if (other.tag == "Enemy") {
+			//dirty dirty game over
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHit : MonoBehaviour {
+
+	public GameObject player;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		transform.parent.GetComponent <PlayerCollision> ().Collision(other);
+	}
+}
