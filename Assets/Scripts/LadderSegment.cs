7f0f81fe6d7118f47559c1d734e66bd93bf605e4
Kasper Mol <awsumthx@gmail.com>
2016-04-17 16:41:22 +0200

    Some fixes

--- Assets/Scripts/LadderSegment.cs
+++ Assets/Scripts/LadderSegment.cs
@@ -5,12 +5,12 @@ public class LadderSegment : MonoBehaviour {
 	void Update () {
 		SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
 
-		if (spriteRenderer.isVisible) {
+//		if (spriteRenderer.isVisible) {
 			transform.localPosition = new Vector3 (
 				transform.localPosition.x,
 				transform.localPosition.y,
 				Mathf.Lerp(10, -9, Camera.main.WorldToViewportPoint(transform.position).y)
 			);
-		}
+//		}
 	}
 }
