d17ba4a7ef8c94d8095807e2df0741b3d575d109
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-04-17 11:59:47 +0200

    Updated birds to fly actually at you, and randomly, and in an angle.

--- Assets/Scripts/BirdEnemy.cs
+++ Assets/Scripts/BirdEnemy.cs
@@ -3,97 +3,32 @@ using System.Collections;
 
 
 public class BirdEnemy : Enemy {
-
-	//the point from where the enemies move
-	//probably defaults to world but some enemies will have the ladder or player as a relative movement position
-	public Vector3      relativeMovementPosition;
-//	public GameObject   enemyPrefab;
-//	public EnemyManager enemyManager;
-//
-	public Vector2 speed            = new Vector2(.25f, .25f);
-	//the default enemy moves right and up
-	public Vector2 relativeMovement = new Vector2 (0, 0);
-
-
-	private int randomBehaviorIndex;
-	private Vector2[] movements = {
-		new Vector2 (1f, -0.325f),
-		new Vector2 (1f, -.75f),
-		new Vector2 (-1f, -0.325f),
-		new Vector2 (-1f, -.75f),
-		new Vector2 (1.2f, -.001f),
-		new Vector2 (-1.2f, .001f),
-		new Vector2 (0.05f, 0.5f),
-		new Vector2 (0.08f, .5f),
-		new Vector2 (-0.09f, .5f)
-	};
-	private Vector2[] spawnPointsRelativeToPlayer = {
-		new Vector2 (-10f, 15f),
-		new Vector2 (-5f, 15f),
-		new Vector2 (5f, 15f),
-		new Vector2 (10f, 15f),
-		new Vector2 (-15f, 5f),
-		new Vector2 (15f, 5f),
-		new Vector2 (4f, 15f),
-		new Vector2 (-5f, 15f),
-		new Vector2 (7f, 15f)
-	};
-
-	//give the enemy some time to be removed it might pop back in frame
-	private SpriteRenderer spriteRenderer;
+	
+	public float speed = 0;
 
 	public GameObject playerObject;
+	private GameObject sprite;
 
-	// Use this for initialization
-	void Start() {
-		return;
-		DetermineRelativeMovementPosition();
-		DetermineSpawnPoint ();
-		if (movements [randomBehaviorIndex].x < 0) {
-			spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
-			spriteRenderer.transform.localScale = new Vector2 (
-				spriteRenderer.transform.localScale.x * -1,
-				spriteRenderer.transform.localScale.y
-			);
+	public void AdjustSpriteToRotation() {
+		sprite = transform.GetChild (0).gameObject;
+
+		float angle = transform.eulerAngles.z;
+
+		Vector3 angles = sprite.transform.localEulerAngles;
+		angles.z = 90;
+		sprite.transform.localEulerAngles = angles;
+
+		if (angle < 180) {
+			Vector3 newScale = sprite.transform.localScale;
+			newScale.y *= -1;
+			sprite.transform.localScale = newScale;
 		}
-	}
-
-	public virtual void DetermineRelativeMovementPosition() {
-		//relativeMovementPosition = transform.TransformPoint (0,0,0);
-	}
-
-	public virtual void DetermineSpawnPoint() {
-		randomBehaviorIndex = Random.Range (0, spawnPointsRelativeToPlayer.Length);
-		transform.position = transform.TransformPoint (
-			playerObject.transform.position.x + spawnPointsRelativeToPlayer[randomBehaviorIndex].x,
-			playerObject.transform.position.y + spawnPointsRelativeToPlayer[randomBehaviorIndex].y,
-			0
-		);
-	}
-
-	public virtual void UpdateRelativeMovementPosition() {
-		//bird enemies are relative to world don't need to update anything
-	}
-
-	public virtual void Move() {
-		//basic movement
-		transform.position = GetNextPosition();
-	}
-
-	public virtual Vector2 GetNextPosition() {
-		
-		return relativeMovementPosition = new Vector2 (
-			transform.position.x + movements[randomBehaviorIndex].x * speed.x,
-			transform.position.y + movements[randomBehaviorIndex].y * speed.y
-		);
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		return;
-		UpdateRelativeMovementPosition ();
-		Move ();
+		transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0), Space.Self);
 	}
 
 
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,11 +10,28 @@ public class EnemyManager : MonoBehaviour
 
 	void Update() {
 		birds.UpdateTimer (Time.deltaTime, (spawner) => {
-			Vector3 position = playerObject.transform.position + new Vector3(15,0,0);
-			GameObject enemy = Instantiate(spawner.enemies[0], position, Quaternion.identity) as GameObject;
+			Vector3 position = playerObject.transform.position + new Vector3(0,0,0);
+			var enemy = Instantiate(spawner.enemies[0], position, Quaternion.identity) as GameObject;
 
-			enemy.GetComponent<BirdEnemy> ().playerObject = playerObject;
-			// Determine position and rotation
+			var enemyScript = enemy.GetComponent<BirdEnemy>();
+			enemyScript.playerObject = playerObject;
+
+			float randomAngle = Random.Range(-40, 30) + 90;
+			if (Random.value < 0.5) randomAngle += 180;
+
+			Vector3 angles = enemyScript.transform.eulerAngles;
+			angles.z = randomAngle;
+			enemyScript.transform.eulerAngles = angles;
+
+			enemyScript.transform.Translate(new Vector3(0, -20, 0), Space.Self);
+
+			float randomPositionalDeviation = Random.Range(-30, 30);
+			enemyScript.transform.position += new Vector3(0, randomPositionalDeviation, 0);
+
+			enemyScript.AdjustSpriteToRotation();
+
+			float speed = Random.Range(8, 15);
+			enemyScript.speed = speed;
 		});
 	}
 
